877b8b0d4ced3e2f381b0c555e888d9834c1be57
Tatsuya Kitajima <dogfoodinglab@gmail.com>
2021-02-13 20:47:34 +0900

    update

--- 00run.sh
+++ 00run.sh
@@ -3,10 +3,7 @@ set -eu
 cd $(dirname $0)
 
 #ansible all -i inventories/develop -m ping -u root --ask-pass
-ansible-playbook -i inventories/develop system-auth.yml -u root --ask-pass
-ansible-playbook -i inventories/develop system-update.yml
-ansible-playbook -i inventories/develop system-config.yml
+
+ansible-playbook -i inventories/develop system-init-minimal.yml -u root --ask-pass
 
 ansible-playbook -i inventories/develop setup-developer.yml
-
-ansible-playbook -i inventories/develop setup-sqlite-3.34.yml
