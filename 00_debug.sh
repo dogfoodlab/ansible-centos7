6c925fdf80151bb6e954ec241d999244a5608a91
Tatsuya Kitajima <dogfoodinglab@gmail.com>
2021-02-20 19:32:33 +0900

    update

--- 00_debug.sh
+++ 00_debug.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 
 #ansible all -i inventories/develop/ -m ping
 #ansible-playbook -i inventories/develop/ init-minimal.yml -u root --ask-pass
+#ansible-playbook -i inventories/develop/ setup-proxy.yml
 
-#ansible-playbook -i inventories/develop/ setup-system.yml
-
+ansible-playbook -i inventories/develop/ setup-system.yml
 ansible-playbook -i inventories/develop/ setup-developer.yml
--- 01_staging.sh
+++ 01_staging.sh
@@ -4,6 +4,5 @@ cd $(dirname $0)
 
 #ansible all -i inventories/staging -m ping
 
-#ansible-playbook -i inventories/staging/ setup-system.yml
-
+ansible-playbook -i inventories/staging/ setup-system.yml
 ansible-playbook -i inventories/staging/ setup-developer.yml
