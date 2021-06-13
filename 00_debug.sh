a27252466850ac3f7c7b3c8d5d7eabb63d09b520
Tatsuya Kitajima <dogfoodinglab@gmail.com>
2021-06-13 16:18:16 +0900

    update

--- 00_debug.sh
+++ 00_debug.sh
@@ -2,9 +2,10 @@
 set -eu
 cd $(dirname $0)
 
-#ansible all -i inventories/develop/ -m ping
-#ansible all -i inventories/develop/ -m setup
+#ansible -i inventories/develop/ all -m ping
+#ansible -i inventories/develop/ all -m setup
 #ansible-playbook -i inventories/develop/ init-minimal.yml -u root --ask-pass
+#ansible-playbook -i inventories/develop/ init-minimal.yml -u ubuntu --ask-pass --ask-become-pass
 #ansible-playbook -i inventories/develop/ setup-proxy.yml
 
 ansible-playbook -i inventories/develop/ setup-system.yml
